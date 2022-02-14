de16865d43fef7ef8d05fed71ba0ee54fb379167
ayerose <reya-k@outlook.de>
2022-02-14 19:39:23 +0000

    balloon functionality

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-document.getElementById("btn").addEventListener("click", calculate)
-let costEl = document.getElementById("cost-el")
-let food = document.getElementById("food-select")
-let transport = document.getElementById("transport-select")
- 
-
- 
+document.getElementById('btn').addEventListener('click', calculate);
+let costEl = document.getElementById('cost-el');
+let food = document.getElementById('food-select');
+let transport = document.getElementById('transport-select');
 
 function calculate() {
-    // 1. Write the JavaScript to calculate the total cost of the selected options from the form.
-    // 2. Display the total cost in the cost-el span.
-  let foodCost = parseInt(food.value) 
-   let transportCost = parseInt(transport.value) 
- let totalCost = foodCost + transportCost
-// console.log(totalCost)
- costEl.innerText = totalCost
+  // 1. Write the JavaScript to calculate the total cost of the selected options from the form.
+  // 2. Display the total cost in the cost-el span.
+
+  let foodCost = parseInt(food.value);
+  let transportCost = parseInt(transport.value);
+  let totalCost = foodCost + transportCost;
+
+  var checkBox = document.getElementById('balloon-checkbox');
+
+  if (checkBox.checked == true) {
+    costEl.innerText = totalCost + 5;
+  } else {
+    costEl.innerText = totalCost;
+  }
 }
- 
-calculate()
+
+calculate();
